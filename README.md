28f233d08d354afb95b17a917be3e9a04269ef6a
yapcrun <yapcrun@tutanota.com>
2025-05-22 14:03:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# TypeMaster
+
+TypeMaster is a Python-based input tracker for linux that plays sound effects when keys are pressed. It is designed to enhance the typing experience by associating key presses with customizable sound effects.
+
+## Features
+
+- **Key Sound Effects**: Plays different sound effects for specific keys based on their function.
+- **Customizable Sound Packs**: Supports multiple sound packs stored in the `sounds/` directory.
+- **Key Logging**: Logs the frequency of key presses to a file (`key_log.txt`).
+- **Threaded Sound Playback**: Ensures non-blocking sound playback using threads.
+
+## Project Structure
+
+- `main.py`: The main script that runs the input tracker and sound playback.
+- `sounds/`: Directory containing sound packs. Each pack has subdirectories for key sounds based on their category.
+- `key_log.txt`: File where key press counts are logged.
+
+## Hotkeys
+- **Exit application**: `ctrl + q`
+- **Pause sounds**: `ctrl + l` 
+
+## Requirements
+
+### System
+- `alsa-utils` to run sounds with aplay
+
+### Python
+- Python 3.6+
+- `pynput` library for keyboard event handling.
+
+Install python dependencies using:
+
+```bash
+pip install -r requirements.txt
+```
---
+++ main.py
@@ -0,0 +1,206 @@
+import os
+import time
+import threading
+import subprocess
+from random import choice
+from pynput import keyboard
+
+# TODO: Display key press stats (in a GUI?)
+# TODO: Make a system tray icon?
+# TODO: Make the exit hotkey more complex
+# TODO: Make play_sound() windows compatible
+# TODO: Make the sounds loaded into memory to minimize disk reads
+
+
+print("Typemaster v0.1.0")
+
+SOUNDS = "default"
+DEBUG = False
+DO_PRINT = False
+DO_LOGGING = True
+EXIT_KEY = "q"
+PAUSE_KEY = "l"
+if not DO_LOGGING: print("Logging disabled!!")
+
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+cwd = os.getcwd()
+print(cwd)
+os_type = os.name
+print(os_type)
+
+class Sound:
+    def __init__(self, sounds = SOUNDS):
+        self.sounds = sounds
+        self.tap_sounds = []
+        self.special_sounds = []
+        self.arrow_sounds = []
+        self.enter_sounds = []
+        self.backspace_sounds = []
+        self.space_sounds = []
+        self.play = True
+        self.sound_dir = cwd + "/sounds/" + self.sounds
+
+        for file in os.listdir(self.sound_dir + "/tap"):
+            if file.endswith(".wav"):
+                self.tap_sounds.append(self.sound_dir + "/tap/" + file)
+        for file in os.listdir(self.sound_dir + "/special"):
+            if file.endswith(".wav"):
+                self.special_sounds.append(self.sound_dir + "/special/" + file)
+        for file in os.listdir(self.sound_dir + "/enter"):
+            if file.endswith(".wav"):
+                self.enter_sounds.append(self.sound_dir + "/enter/" + file)
+        for file in os.listdir(self.sound_dir + "/arrow"):
+            if file.endswith(".wav"):
+                self.arrow_sounds.append(self.sound_dir + "/arrow/" + file)
+        for file in os.listdir(self.sound_dir + "/backspace"):
+            if file.endswith(".wav"):
+                self.backspace_sounds.append(self.sound_dir + "/backspace/" + file)
+        for file in os.listdir(self.sound_dir + "/space"):
+            if file.endswith(".wav"):
+                self.space_sounds.append(self.sound_dir + "/space/" + file)
+
+        if DEBUG:
+            print(f"tap sounds: {self.tap_sounds}")
+            print(f"special sounds: {self.special_sounds}")
+            print(f"arrow sounds: {self.arrow_sounds}")
+            print(f"enter sounds: {self.enter_sounds}")
+            print(f"backspace sounds: {self.backspace_sounds}")
+
+
+    def play_sound(self, key_type = "generic"):
+        if not self.play:
+            return
+        if key_type == "special":
+            sound = choice(self.special_sounds)
+        elif key_type == "arrow":
+            sound = choice(self.arrow_sounds)
+        elif key_type == "enter":
+            sound = choice(self.enter_sounds)
+        elif key_type == "backspace":
+            sound = choice(self.backspace_sounds)
+        elif key_type == "space":
+            sound = choice(self.space_sounds)
+        else:
+            sound = choice(self.tap_sounds)
+        
+        subprocess.run("aplay -q " + sound, cwd=cwd, shell=True)
+
+    def ps(self, key_type = "generic"):
+        if key_type == "special":
+            threading.Thread(target=self.play_sound, args=["special"]).start()
+        elif key_type == "arrow":
+            threading.Thread(target=self.play_sound, args=["arrow"]).start()
+        elif key_type == "enter":
+            threading.Thread(target=self.play_sound, args=["enter"]).start()
+        elif key_type == "backspace":
+            threading.Thread(target=self.play_sound, args=["backspace"]).start()
+        elif key_type == "space":
+            threading.Thread(target=self.play_sound, args=["space"]).start()
+        else:
+            threading.Thread(target=self.play_sound).start()
+
+
+class InputTracker:
+    def __init__(self):
+        self.sound = Sound()
+        self.key_dict = {}
+        self.load_keys()
+        self.last_save_time = time.time()
+        self.mod_key_pressed = False
+
+        
+    def on_press(self, key):
+        if DEBUG: print(key.__dict__)
+        try:
+            if hasattr(key, 'char') and key.char is not None: # if is a character key
+
+                # Hotkey to toggle sound
+                if self.mod_key_pressed and key.char == PAUSE_KEY:
+                    self.sound.play = not self.sound.play
+                    if self.sound.play:
+                        print("Sound enabled")
+                    else:
+                        print("Sound disabled")
+                    return
+                elif self.mod_key_pressed and key.char == EXIT_KEY:
+                    self.save_keys()
+                    exit(0)
+
+
+                if DO_LOGGING: self.key_dict[key.char] = self.key_dict.get(key.char, 0) + 1
+                if DO_PRINT: print(f'Key pressed: {key.char}')
+
+
+                self.sound.ps()
+                
+            elif hasattr(key, 'name') and key.name is not None: # if is a special key\
+                # Handle modifier toggle
+                if key == keyboard.Key.ctrl:
+                    self.mod_key_pressed = True
+                
+                if key.name in ["up", "down", "left", "right"]:
+                    self.sound.ps(key_type="arrow")
+                elif key.name == "space":
+                    self.sound.ps(key_type="space")
+                elif key.name == "enter":
+                    self.sound.ps(key_type="enter")
+                elif key.name == "backspace":
+                    self.sound.ps(key_type="backspace")
+                else:
+                    self.sound.ps(key_type="special")
+                
+                if DO_LOGGING: self.key_dict[str(key.name)] = self.key_dict.get(str(key.name), 0) + 1
+                if DO_PRINT: print(f'Special key pressed: {key.name}')
+
+        except Exception as e:
+            print(f"Error in on_press: {e}")
+
+        if time.time() - self.last_save_time > 60:
+            if DO_LOGGING: self.save_keys()
+            self.last_save_time = time.time()
+                
+    def on_release(self, key):
+        if DEBUG: print(f"Release:{key}")
+        try:
+            if key == keyboard.Key.ctrl:
+                self.mod_key_pressed = False
+        except Exception as e:
+            print(f"Error in on_release: {e}")
+
+    def save_keys(self):
+        try:
+            with open('key_log.txt', 'w') as f:
+                for key, count in self.key_dict.items():
+                    f.write(f'{key}: {count}\n')
+            print("Keys saved to key_log.txt")
+        except Exception as e:
+            print(f"Error saving keys: {e}")
+
+    def load_keys(self):
+        try:
+            if os.path.exists('key_log.txt'):
+                with open('key_log.txt', 'r') as f:
+                    for line in f:
+                        key, count = line.strip().split(': ')
+                        self.key_dict[key] = int(count)
+                print("Keys loaded from key_log.txt")
+            else:
+                print("No previous key log found.")
+        except Exception as e:
+            print(f"Error loading keys: {e}")
+
+
+
+input_tracker = InputTracker()
+with keyboard.Listener(
+        on_press=input_tracker.on_press,
+        on_release=input_tracker.on_release
+) as listener:
+    try:
+        listener.join()
+    except KeyboardInterrupt:
+        print("Listener stopped.")
+        os._exit(0)
+    except Exception as e:
+        print(f"An error occurred: {e}")
